0a2fe5610b7f2d44088585d0edf640bcd3d5ebc2
SamNilsson172 <55138241+SamNilsson172@users.noreply.github.com>
2019-09-12 14:13:17 +0200

    added bar animation method, unused tho

--- Dogimon/Assets/Scripts/Battle/UpdateDogVisuals.cs
+++ Dogimon/Assets/Scripts/Battle/UpdateDogVisuals.cs
@@ -31,7 +31,24 @@ public class UpdateDogVisuals : MonoBehaviour
         opponentDog.sprite = fight.opponentDog.dogimon.imgFront;
         playerNameAndLvl.text = fight.playerDog.nickname + " Lvl. " + fight.playerDog.lvl;
         opponentNameAndLvl.text = fight.opponentDog.nickname + " Lvl. " + fight.opponentDog.lvl;
-        playerHp.text = fight.playerDog.currentHp + " / " + fight.playerDog.dogimon.hp;
-        OpponentHp.text = fight.opponentDog.currentHp + " / " + fight.opponentDog.dogimon.hp;
+        playerHp.text = Mathf.Ceil(fight.playerDog.currentHp) + " / " + fight.playerDog.dogimon.hp;
+        OpponentHp.text = Mathf.Ceil(fight.opponentDog.currentHp) + " / " + fight.opponentDog.dogimon.hp;
+    }
+
+    public IEnumerator BarAnimation(bool playerHp, bool xp, float max, float current, float modifier)
+    {
+        Image bar = null;
+        if (playerHp)
+            bar = playerHpBar;
+        if (!playerHp)
+            bar = opponentHpBar;
+        if (xp)
+            bar = xpBar;
+
+        for (float f = 0; f != 1; f += 0.025f)
+        {
+            bar.transform.localScale = new Vector2((current - modifier * f) / max, 1);
+            yield return new WaitForSeconds(0.025f); //once every 40th sec
+        }
     }
 }
